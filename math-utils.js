d5ea6c2d1d376d8c62d270c6899ab43ec8b0bc91
Luke Codewalker <arathorn089@arcor.de>
2018-05-18 11:45:52 +0200

    added more math functions
    added a limit, map and norm function
    limit -> limits a number between to numbers and clamps it if necessary
    map -> maps a number from one range to another
    norm -> normalizes a number from a given range to [0,1]

--- math-utils.js
+++ math-utils.js
@@ -7,3 +7,31 @@ const random = (min = 1, max = 0) => {
     const x = Math.random() * Math.abs(max - min);
     return max != 0 ? min + x : min - x;
 }
+
+// function to limit a number between lower and higher bound
+const limit = (n, low, high) => {
+    if(n > low && n < high) {
+        return n;
+    } else if (n < low ) {
+        return low;
+    } else if (n > high) {
+        return high;
+    }
+}
+
+// function to map a number from one range into another
+const map = (n, start1, end1, start2, end2, limited = false) => {
+    const newVal = ((n - start1)/ (end1 -start1)) * (end2 - start2) + start2;
+
+    if(!limited) {
+        return newVal;
+    } else if (start2 < end2) {
+        return limit(newVal, start2, end2);
+    } else {
+        return limit(newVal, end2, start2);
+    }
+}
+
+// function to normalize a number from a range to a floating point number between 0 and 1
+// just a shorthand for map(n, low, high, 0, 1)
+const norm = (n, low, high, limited = false) => map(n, low, high, 0, 1, limited)
